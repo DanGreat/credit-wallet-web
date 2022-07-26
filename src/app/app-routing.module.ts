e5051805dad94380ffe4a13b506e7620a81b6bb5
Daniel Bassey <bassey.daniel@wizerconsulting.com>
2022-07-26 14:30:38 +0100

    First push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule)
+  },
+  {
+    path: 'verification',
+    loadChildren: () => import('./pages/verification/verification.module').then(m => m.VerificationModule)
+  },
+  {
+    path: 'loan',
+    loadChildren: () => import('./pages/loan-disbursement/loan-disbursement.module').then(m => m.LoanDisbursementModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './shared-component/sidebar/sidebar.component';
+import { HeaderComponent } from './shared-component/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
